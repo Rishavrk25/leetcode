2bf5e26233d7ff8685afb413539f82accb4d0435
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-01-23 11:54:11 +0530

    Time: 2509 ms (5.00%) | Memory: 48.1 MB (16.04%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,24 +1,38 @@
 class Solution {
-    public boolean canJump(int[] arr) {
-        // Brute Force
-        int n=arr.length;
-        if(n==1) return true;
-        boolean iszero=false;
-        for(int i=0;i<n-1;i++) if(arr[i]==0) iszero=true;
-        if(!iszero) return true;
-        for(int i=0;i<n-1;i++){
-            if(arr[i]==0){
-                boolean flag=false;
-                for(int j=i-1;j>=0;j--){
-                    if(j+arr[j]>i){
-                        flag=true;
-                        break;
-                    }
-                }
-                if(!flag) return false;
-            }
+    boolean helper(int arr[],int i,int n,int dp[]){
+        if(i==n-1) return true;
+        if(dp[i]!=-1) return dp[i]==1;
+        boolean flag=false;
+        for(int j=1;j<=arr[i];j++){
+            flag=flag||helper(arr,i+j,n,dp);
         }
-        return true;
+        dp[i]=flag?1:0;
+        return flag;
+    }
+    public boolean canJump(int[] arr) {
+        int n=arr.length;
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        return helper(arr,0,n,dp);
+        // Brute Force
+        // int n=arr.length;
+        // if(n==1) return true;
+        // boolean iszero=false;
+        // for(int i=0;i<n-1;i++) if(arr[i]==0) iszero=true;
+        // if(!iszero) return true;
+        // for(int i=0;i<n-1;i++){
+        //     if(arr[i]==0){
+        //         boolean flag=false;
+        //         for(int j=i-1;j>=0;j--){
+        //             if(j+arr[j]>i){
+        //                 flag=true;
+        //                 break;
+        //             }
+        //         }
+        //         if(!flag) return false;
+        //     }
+        // }
+        // return true;
 
         // Greedy
         // int n=arr.length;
