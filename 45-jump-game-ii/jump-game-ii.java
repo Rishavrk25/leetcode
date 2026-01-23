475d55afe8436974422a524cc9654f5901fffd07
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-01-23 12:37:18 +0530

    Time: 1 ms (99.53%) | Memory: 47.7 MB (5.35%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,17 +1,18 @@
 class Solution {
-    int helper(int arr[],int i,int n,int dp[]){
-        if(i>=n-1) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int c=n-1;
-        for(int j=1;j<=arr[i];j++){
-            c=Math.min(c,1 + helper(arr,i+j,n,dp));
-        }
-        return dp[i]=c;
-    }
     public int jump(int[] arr) {
         int n=arr.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp,-1);
-        return helper(arr,0,n,dp);
+        int l=0;
+        int r=0;
+        int jumps=0;
+        while(r<n-1){
+            int farthest = 0;
+            for(int i=l;i<=r;i++){
+                farthest = Math.max(farthest,i+arr[i]);
+            }
+            l=r+1;
+            r=farthest;
+            jumps++;
+        }
+        return jumps;
     }
 }
