0e48e021a54bd08841b6774e9410fc5eb3c3b2d5
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-01-23 11:42:00 +0530

    Time: 2 ms (85.09%) | Memory: 47.8 MB (71.26%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,12 +1,33 @@
 class Solution {
     public boolean canJump(int[] arr) {
+        // Brute Force
         int n=arr.length;
-        int maxIdx  = 0;
-        for(int i=0;i<n;i++){
-            if(maxIdx<i) return false;
-            maxIdx = Math.max(maxIdx,i+arr[i]);
+        if(n==1) return true;
+        boolean iszero=false;
+        for(int i=0;i<n-1;i++) if(arr[i]==0) iszero=true;
+        if(!iszero) return true;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]==0){
+                boolean flag=false;
+                for(int j=i-1;j>=0;j--){
+                    if(j+arr[j]>i){
+                        flag=true;
+                        break;
+                    }
+                }
+                if(!flag) return false;
+            }
         }
         return true;
+
+        // Greedy
+        // int n=arr.length;
+        // int maxIdx  = 0;
+        // for(int i=0;i<n;i++){
+        //     if(maxIdx<i) return false;
+        //     maxIdx = Math.max(maxIdx,i+arr[i]);
+        // }
+        // return true;
         
     }
 }
