f99d6808e9750ccfd65fdab9421130975b6ca36c
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-01-27 01:55:21 +0530

    Time: 3 ms (54.24%) | Memory: 45 MB (84.77%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,22 +1,19 @@
 class Solution {
-    void helper(int arr[],int i,List<Integer> list,Set<List<Integer>> set){
-        if(i==arr.length){
-            set.add(new ArrayList<>(list));
-            return;
+    void helper(int arr[],int idx,List<Integer> list ,List<List<Integer>> ans){
+        ans.add(new ArrayList<>(list));
+        for(int i=idx;i<arr.length;i++){
+            if(i>idx && arr[i]==arr[i-1]) continue;
+            list.add(arr[i]);
+            helper(arr,i+1,list,ans);
+            list.remove(list.size()-1);
         }
-        // take
-        list.add(arr[i]);
-        helper(arr,i+1,list,set);
-        list.remove(list.size()-1); // backtrack
-        // skip
-        helper(arr,i+1,list,set);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        helper(nums,0,list,set);
-        List<List<Integer>> ans = new ArrayList<>(set);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        helper(nums,0,list,ans);
         return ans;
+        
     }
 }
