87f04c8ed8abc512f1a0cac30b38f181b3ea45b5
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-01-26 15:30:48 +0530

    Time: 7 ms (40.06%) | Memory: 45.4 MB (33.88%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,26 +1,22 @@
 class Solution {
-    void helper(int arr[],int t,int i,int sum,List<Integer> list,Set<List<Integer>> set){
-        if(i==arr.length){
-            List<Integer> newlist= new ArrayList<>(list);
-            if(sum==t && !set.contains(newlist)) set.add(newlist);
+    void helper(int arr[],int t,int idx,List<Integer> list,List<List<Integer>> ans){
+        if(t<0) return;
+        if(t==0){
+            ans.add(new ArrayList<>(list));
             return;
         }
-        // take
-        list.add(arr[i]);
-        if(sum+arr[i]<=t) helper(arr,t,i+1,sum+arr[i],list,set);
-        list.remove(list.size()-1);
-        // skip all duplicates of current element
-        int j = i;
-        while (j < arr.length && arr[j] == arr[i]) j++;
-        // skip
-        helper(arr,t,j,sum,list,set);
+        for(int i=idx;i<arr.length;i++){
+            if(i>idx && arr[i]==arr[i-1]) continue;
+            list.add(arr[i]);
+            helper(arr,t-arr[i],i+1,list,ans);
+            list.remove(list.size()-1);
+        }
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        helper(candidates,target,0,0,list,set);
-        List<List<Integer>> ans =new ArrayList<>(set);
+        helper(candidates,target,0,list,ans);
         return ans;
     }
 }
