f68f06a15086808b9ade572766e9ba7602c1a896
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-01-27 17:36:02 +0530

    Time: 664 ms (5.00%) | Memory: 47.4 MB (5.86%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean helper(char arr[][],int i,int j,String word,String s,int k,boolean visited[][],int n,int m){
+        if(i<0 || j<0 || i>=n || j>=m || visited[i][j] || word.charAt(k)!=arr[i][j] ) return false;
+        s+=word.charAt(k);
+        if(s.equals(word)) return true;
+        visited[i][j]=true;
+        int direction[][]={{0,-1},{0,1},{1,0},{-1,0}};
+        boolean ans = false;
+        for(int dir=0;dir<4;dir++){
+            int i_=i+direction[dir][0];
+            int j_=j+direction[dir][1];
+            ans = ans || helper(arr,i_,j_,word,s,k+1,visited,n,m);
+        }
+        visited[i][j]=false;
+
+        return ans;
+    }
+    public boolean exist(char[][] board, String word) {
+        int n=board.length;
+        int m=board[0].length;
+        boolean visited[][] = new boolean[n][m];
+        boolean ans = false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word.charAt(0)){
+                    ans = ans || helper(board,i,j,word,"",0,visited,n,m);
+                }
+            }
+        }
+        return ans;
+    }
+}
