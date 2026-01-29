9ac6f745c05407c76058cdd91fb781dea4a2bf03
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-01-29 20:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (36.14%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,34 +1,19 @@
-class Pair{
-    int i;
-    int j;
-    Pair(int i,int j){
-        this.i=i;
-        this.j=j;
-    }
-}
 class Solution {
-        void bfs(int arr[][],int i,int j,int color){
+       void dfs(int arr[][],int i,int j,int newColor,int oldColor){
         int n=arr.length;
         int m=arr[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        int oldcolor = arr[i][j];
-        if (oldcolor == color) return;
-        arr[i][j]=color;
-        q.add(new Pair(i,j));
+        if(i<0 || i>=n || j<0 || j>=m || arr[i][j]!=oldColor) return;
+        arr[i][j]=newColor;
         int direction[][]={{0,1},{0,-1},{1,0},{-1,0}};
-        while(!q.isEmpty()){
-            Pair p = q.remove();
-            for(int dir=0;dir<4;dir++){
-                int i_ = p.i+direction[dir][0];
-                int j_ = p.j+direction[dir][1];
-                if(i_<0 || i_>=n || j_<0 || j_>=m || arr[i_][j_]!=oldcolor) continue;
-                q.add(new Pair(i_,j_));            
-                arr[i_][j_]=color;
-            }
+        for(int dir=0;dir<4;dir++){
+            int i_ = i + direction[dir][0];
+            int j_ = j + direction[dir][1];
+            dfs(arr,i_,j_,newColor,oldColor);
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        bfs(image,sr,sc,color);
+        if(image[sr][sc]==color) return image;
+        dfs(image,sr,sc,color,image[sr][sc]);
         return image;
     }
 }
