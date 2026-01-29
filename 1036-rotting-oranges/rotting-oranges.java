6c97f713ab710a4d31d6ee1ae81fb11b069e2b07
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-01-29 11:12:32 +0530

    Time: 2 ms (90.77%) | Memory: 44.3 MB (34.96%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,42 @@
+class Pair{
+    int i;
+    int j;
+    Pair(int i,int j){
+        this.i=i;
+        this.j=j;
+    }
+}
+class Solution {
+    public int orangesRotting(int[][] adj) {
+
+        Queue<Pair> q = new LinkedList<>();
+        int n=adj.length;
+        int m=adj[0].length;
+        int fresh=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(adj[i][j]==2) q.add(new Pair(i,j));
+                if(adj[i][j]==1) fresh++;
+            }
+        }
+        int direction[][]={{0,1},{0,-1},{1,0},{-1,0}};
+        int c=0;
+        while(!q.isEmpty() && fresh>0){
+            int size=q.size();
+            c++;
+            for(int i=0;i<size;i++){
+                Pair p = q.remove();
+                for(int dir=0;dir<4;dir++){
+                    int i_ = p.i + direction[dir][0];
+                    int j_ = p.j + direction[dir][1];
+                    if(i_<0 || i_>=n || j_<0 || j_>=m || adj[i_][j_]!=1) continue;
+                    adj[i_][j_]=2;
+                    fresh--;
+                    q.add(new Pair(i_,j_));
+                }
+            }
+        }
+        return (fresh==0)?c:-1;
+        
+    }
+}
