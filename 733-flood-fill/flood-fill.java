07a1400440d32561436bfeb6c45c38c7455c4bc5
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-01-29 19:16:04 +0530

    Time: 2 ms (12.57%) | Memory: 47.3 MB (8.20%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,34 @@
+class Pair{
+    int i;
+    int j;
+    Pair(int i,int j){
+        this.i=i;
+        this.j=j;
+    }
+}
+class Solution {
+        void bfs(int arr[][],int i,int j,int color){
+        int n=arr.length;
+        int m=arr[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+        int old = arr[i][j];
+        arr[i][j]=color;
+        q.add(new Pair(i,j));
+        int direction[][]={{0,1},{0,-1},{1,0},{-1,0}};
+        while(!q.isEmpty()){
+            Pair p = q.remove();
+            for(int dir=0;dir<4;dir++){
+                int i_ = p.i+direction[dir][0];
+                int j_ = p.j+direction[dir][1];
+                if(i_<0 || i_>=n || j_<0 || j_>=m || arr[i_][j_]!=old || arr[i_][j_]==color) continue;
+                q.add(new Pair(i_,j_));            
+                arr[i_][j_]=color;
+            }
+        }
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        bfs(image,sr,sc,color);
+        return image;
+    }
+}
