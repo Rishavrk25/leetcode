a8e6d09db36f95f92c92b0c03962afeb2372882b
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-01 00:55:02 +0530

    Time: 2 ms (82.53%) | Memory: 47.7 MB (67.62%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,20 +1,14 @@
 class Solution {
-    void bfs(char arr[][],int i,int j,boolean visited[][]){
+    void dfs(char arr[][],int i,int j,boolean visited[][]){
         int n=arr.length;
         int m=arr[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i,j});
         visited[i][j]=true;
         int direction[][] = {{-1,0},{1,0},{0,1},{0,-1}};
-        while(!q.isEmpty()){
-            int p[] = q.remove();
-            for(int dir=0;dir<4;dir++){
-                int i_=p[0]+direction[dir][0];
-                int j_=p[1]+direction[dir][1];
-                if(i_<0 || i_>=n || j_<0 || j_>=m || visited[i_][j_] || arr[i_][j_]!='O') continue;
-                q.add(new int[]{i_,j_});
-                visited[i_][j_]=true;
-            }
+        for(int dir = 0;dir<4;dir++){
+            int i_ = i+direction[dir][0];
+            int j_ = j+direction[dir][1];
+            if(i_<0 || i_>=n || j_<0 || j_>=m || visited[i_][j_] || arr[i_][j_]!='O') continue;
+            dfs(arr,i_,j_,visited);
         }
     }
     public void solve(char[][] arr) {
@@ -23,15 +17,15 @@ class Solution {
         boolean visited[][] = new boolean[n][m];
         for(int i=0;i<n;i++){
             // first col
-            if(arr[i][0]=='O' && !visited[i][0]) bfs(arr,i,0,visited);
+            if(arr[i][0]=='O' && !visited[i][0]) dfs(arr,i,0,visited);
             // last col
-            if(arr[i][m-1]=='O' && !visited[i][m-1]) bfs(arr,i,m-1,visited);
+            if(arr[i][m-1]=='O' && !visited[i][m-1]) dfs(arr,i,m-1,visited);
         }
         for(int j=0;j<m;j++){
             // first row
-            if(arr[0][j]=='O' && !visited[0][j]) bfs(arr,0,j,visited);
+            if(arr[0][j]=='O' && !visited[0][j]) dfs(arr,0,j,visited);
             // last row
-            if(arr[n-1][j]=='O' && !visited[n-1][j]) bfs(arr,n-1,j,visited);
+            if(arr[n-1][j]=='O' && !visited[n-1][j]) dfs(arr,n-1,j,visited);
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
