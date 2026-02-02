a74b667ceb364e0baaf48b4d8335fdae48aee6ee
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-02 21:25:37 +0530

    Time: 4 ms (92.66%) | Memory: 47.1 MB (74.89%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,4 +1,16 @@
 class Solution {
+    boolean isCycle(List<List<Integer>> adj,int i,boolean visited[],boolean inRecursion[]){
+        visited[i]=true;
+        inRecursion[i]=true;
+        for(int nei:adj.get(i)){
+            if(!visited[nei]){
+                if(isCycle(adj,nei,visited,inRecursion)) return true;
+            }
+            else if(inRecursion[nei]) return true;
+        }
+        inRecursion[i]=false;
+        return false;
+    }
     public boolean canFinish(int n, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<n;i++) adj.add(new ArrayList<>());
@@ -9,20 +21,27 @@ class Solution {
             adj.get(v).add(u);
             indegree[u]++;
         }
-        Queue<Integer> q = new LinkedList<>();
+        boolean visited[] = new boolean[n];
+        boolean inRecursion[] = new boolean[n];
         for(int i=0;i<n;i++){
-            if(indegree[i]==0) q.add(i);
+            if(!visited[i] && isCycle(adj,i,visited,inRecursion)) return false;
         }
-        int c=0;
-        while(!q.isEmpty()){
-            int u=q.remove();
-            c++;
-            for(int nei:adj.get(u)){
-                indegree[nei]--;
-                if(indegree[nei]==0) q.add(nei);
-            }
-        }
-        if(c==n) return true;
-        else return false;
+        return true;
+        
+        // Queue<Integer> q = new LinkedList<>();
+        // for(int i=0;i<n;i++){
+        //     if(indegree[i]==0) q.add(i);
+        // }
+        // int c=0;
+        // while(!q.isEmpty()){
+        //     int u=q.remove();
+        //     c++;
+        //     for(int nei:adj.get(u)){
+        //         indegree[nei]--;
+        //         if(indegree[nei]==0) q.add(nei);
+        //     }
+        // }
+        // if(c==n) return true;
+        // else return false;
     }
 }
