4bcfa1736a721885dc197e89d46d26600b8c5900
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-02 14:24:40 +0530

    Time: 1 ms (61.59%) | Memory: 47.3 MB (31.43%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,41 +1,40 @@
 class Solution {
-    boolean dfs(int graph[][],int color[],int i){
-        // color[i]=0;
-        for(int nei : graph[i]){
-            if(color[nei]==-1){
-                color[nei] = 1-color[i];
-                if(!dfs(graph,color,nei)) return false;
-            }
-            else if(color[nei]==color[i]) return false;
-        }
-        return true;
-    }
-    // boolean bfs(int graph[][],int color[],int i){
-    //     Queue<Integer> q = new LinkedList<>();
-    //     q.add(i);
-    //     color[0]=0;
-    //     while(!q.isEmpty()){
-    //         int u=q.remove();
-    //         for(int nei : graph[u]){
-    //             if(color[nei]==-1){
-    //                 color[nei] = 1 - color[u];
-    //                 q.add(nei);
-    //             }
-    //             else{
-    //                 if(color[nei]==color[u]) return false;
-    //             }
+    // boolean dfs(int graph[][],int color[],int i){
+    //     for(int nei : graph[i]){
+    //         if(color[nei]==-1){
+    //             color[nei] = 1-color[i];
+    //             if(!dfs(graph,color,nei)) return false;
     //         }
+    //         else if(color[nei]==color[i]) return false;
     //     }
     //     return true;
     // }
+    boolean bfs(int graph[][],int color[],int i){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        color[0]=0;
+        while(!q.isEmpty()){
+            int u=q.remove();
+            for(int nei : graph[u]){
+                if(color[nei]==-1){
+                    color[nei] = 1 - color[u];
+                    q.add(nei);
+                }
+                else{
+                    if(color[nei]==color[u]) return false;
+                }
+            }
+        }
+        return true;
+    }
     public boolean isBipartite(int[][] graph) {
         int n=graph.length;
         int color[] = new int[n];
         Arrays.fill(color,-1);
         for(int i=0;i<n;i++){
             if(color[i]==-1){
-                // if(!bfs(graph,color,i)) return false;
-                if(!dfs(graph,color,i)) return false;
+                if(!bfs(graph,color,i)) return false;
+                // if(!dfs(graph,color,i)) return false;
             }
         }
         return true;
