0f226aea3ac42eee3657d170cc490532c6d014c5
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-02 14:06:08 +0530

    Time: 1 ms (61.59%) | Memory: 47.1 MB (85.78%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,30 @@
+class Solution {
+    boolean bfs(int graph[][],int color[],int i){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        color[0]=0;
+        while(!q.isEmpty()){
+            int u=q.remove();
+            for(int nei : graph[u]){
+                if(color[nei]==-1){
+                    color[nei] = 1 - color[u];
+                    q.add(nei);
+                }
+                else{
+                    if(color[nei]==color[u]) return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isBipartite(int[][] graph) {
+        int n=graph.length;
+        int color[] = new int[n];
+        Arrays.fill(color,-1);
+        for(int i=0;i<n;i++){
+            if(color[i]==-1) if(!bfs(graph,color,i)) return false;
+        }
+        return true;
+
+    }
+}
