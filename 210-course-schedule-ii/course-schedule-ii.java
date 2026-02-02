38d5d284277032e4aa05e7f7975bdf19e1112539
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-02 22:24:07 +0530

    Time: 4 ms (93.90%) | Memory: 47.1 MB (43.81%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,36 +1,39 @@
 class Solution {
+    boolean isCycle(List<List<Integer>> adj,int i,List<Integer> list,boolean visited[],boolean inRecursion[]){
+        visited[i]=true;
+        inRecursion[i]=true;
+        for(int nei:adj.get(i)){
+            if(!visited[nei]){
+                if(isCycle(adj,nei,list,visited,inRecursion)) return true;
+            }
+            else if(inRecursion[nei]) return true; 
+        }
+        list.add(0,i);
+        inRecursion[i]=false;
+        return false;
+    }
     public int[] findOrder(int n, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-        int indegree[] = new int[n];
         for(int arr[]:prerequisites){
             int u=arr[0];
             int v=arr[1];
             adj.get(v).add(u);
-            indegree[u]++;
         }
         boolean visited[] = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0) q.add(i);
-        }
+        boolean inRecursion[] = new boolean[n];
         List<Integer> list = new ArrayList<>();
-        while(!q.isEmpty()){
-            int u=q.remove();
-            list.add(u);
-            for(int nei:adj.get(u)){
-                indegree[nei]--;
-                if(indegree[nei]==0) q.add(nei);
+        boolean flag = false; // no cycle
+        for(int i=0;i<n;i++){
+            if(!visited[i] && isCycle(adj,i,list,visited,inRecursion)){
+                flag = true; // cycle
+                break;
             }
         }
-        if(list.size()!=n) return new int[]{};
+        if(flag) return new int[]{};
         int ans[] = new int[n];
-        for(int i=0;i<n;i++){
-            ans[i]=list.get(i);
-        }
+        for(int i=0;i<n;i++) ans[i]=list.get(i);
         return ans;
-        
-        
 
     }
 }
