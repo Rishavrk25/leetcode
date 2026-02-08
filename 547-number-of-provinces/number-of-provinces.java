cee90bf07e3d6e12e8a8e90e71138ec17f53dc02
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-08 12:32:45 +0530

    Time: 2 ms (48.05%) | Memory: 47.3 MB (73.00%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,32 @@
+class Solution {
+    int parent[];
+    int find(int a){
+        if(parent[a]==a) return a;
+        return find(parent[a]);
+    }
+    void union(int a,int b){
+        a = find(a);
+        b = find(b);
+        if(a!=b){
+            parent[b]=a;
+        }
+    }
+    public int findCircleNum(int[][] adj) {
+        int n=adj.length;
+        parent = new int[n+1];
+        for(int i=1;i<n+1;i++){
+            parent[i]=i;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                // edge is from i+1 to j+1
+                if(i!=j && adj[i][j]==1) union(i+1,j+1);
+            }
+        }
+        int c=0;
+        for(int i=1;i<n+1;i++){
+            if(parent[i]==i) c++;
+        }
+        return c;
+    }
+}
