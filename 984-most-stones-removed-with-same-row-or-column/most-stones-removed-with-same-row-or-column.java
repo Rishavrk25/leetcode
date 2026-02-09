17a057ad59ad51b63f3759687a8a16ea2c42e2c4
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-09 15:42:23 +0530

    Time: 23 ms (49.12%) | Memory: 46.9 MB (62.51%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,25 +1,46 @@
 class Solution {
-    void dfs(int arr[][],boolean visited[],int u){
-        visited[u]=true;
-        int r=arr[u][0];
-        int c=arr[u][1];
-        for(int i=0;i<arr.length;i++){
-            int nr=arr[i][0];
-            int nc=arr[i][1];
-            if(!visited[i] && (r==nr || c==nc)){
-                dfs(arr,visited,i);
+    int parent[];
+    int size[];
+    int find(int a){
+        if(parent[a]==a) return a;
+        return parent[a]=find(parent[a]);
+    }
+    void union(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a!=b){
+            if(size[a]>size[b]){
+                parent[b]=a;
+                size[a]+=size[b];
+            }
+            else{
+                parent[a]=b;
+                size[b]+=size[a];
             }
         }
     }
     public int removeStones(int[][] stones) {
         int n=stones.length;
-        boolean visited[] = new boolean[n];
+        parent=new int[n];
+        size=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+        for(int i=0;i<n;i++){
+            int r=stones[i][0];
+            int c=stones[i][1];
+            for(int j=i+1;j<n;j++){
+                int nr=stones[j][0];
+                int nc=stones[j][1];
+                if(r==nr || c==nc){
+                    union(i,j);
+                }
+            }
+        }
         int groups=0;
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(stones,visited,i);
-                groups++;
-            }
+            if(parent[i]==i) groups++;
         }
         return n-groups;
     }
