888e07aa4fea5fee140f9ef703c77b28f9615293
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-10 21:16:46 +0530

    Time: 27 ms (95.31%) | Memory: 49.8 MB (51.27%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,40 +1,54 @@
 class Solution {
-    void dfs(Map<String,List<String>> adjMap,Set<String> visited,String u,List<String> sublist){
-        visited.add(u);
-        sublist.add(u);
-        // if(!adjMap.containsKey(u)){
-        //     return;
-        // }
-        for(String nei: adjMap.get(u)){
-            if(!visited.contains(nei)){
-                dfs(adjMap,visited,nei,sublist);
+    int parent[];
+    int size[];
+    int find(int a){
+        if(parent[a]==a) return a;
+        return parent[a]=find(parent[a]);
+    }
+    void union(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a!=b){
+            if(size[a]>size[b]){
+                parent[b]=a;
+                size[a]+=size[b];
+            }
+            else{
+                parent[a]=b;
+                size[b]+=size[a];
             }
         }
     }
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        Map<String,List<String>> adjMap = new HashMap<>();
-        for(List<String> list : accounts){
-            String firstEmail=list.get(1);
-            if(!adjMap.containsKey(firstEmail)) adjMap.put(firstEmail,new ArrayList<>());
-            for(int i=2;i<list.size();i++){
-                String email=list.get(i);
-                adjMap.get(firstEmail).add(email);
-                if(!adjMap.containsKey(email)) adjMap.put(email,new ArrayList<>());
-                adjMap.get(email).add(firstEmail);
+        int n=accounts.size();
+        parent=new int[n];
+        size=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            List<String> list = accounts.get(i);
+            for(int j=1;j<list.size();j++){
+                String email=list.get(j);
+                if(!map.containsKey(email)) map.put(email,i);
+                else union(i,map.get(email));
             }
         }
+        List<List<String>> merged = new ArrayList<>();
+        for(int i=0;i<accounts.size();i++) merged.add(new ArrayList<>());
+        for(String key:map.keySet()){
+            int val = find(map.get(key));
+            merged.get(val).add(key);
+        }
         List<List<String>> ans = new ArrayList<>();
-        Set<String> visited = new HashSet<>();
-        for(List<String> list : accounts){
-            String firstEmail=list.get(1);
-            if(!visited.contains(firstEmail)){
-                List<String> sublist = new ArrayList<>();
-                dfs(adjMap,visited,firstEmail,sublist);
-                Collections.sort(sublist);
-                sublist.add(0,list.get(0));
-                ans.add(sublist);
-            }
-      
+        for(int i=0;i<accounts.size();i++){
+            List<String> list = merged.get(i);
+            if(list.size()==0) continue;
+            Collections.sort(list);
+            list.add(0,accounts.get(i).get(0));
+            ans.add(list);
         }
         return ans;
     }
