4ed580122be379ca8915c9efabe2f9ddbb9316a8
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-12 17:44:17 +0530

    Time: 104 ms (48.04%) | Memory: 148.2 MB (44.01%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.java
+++ 854-making-a-large-island/making-a-large-island.java
@@ -1,27 +1,44 @@
 class Solution {
-    int dfs(int arr[][],int i,int j,int n,int uniqueId){
-        arr[i][j]=uniqueId;
-        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
-        int size=1;
-        for(int d[]:dir){
-            int ni=i+d[0];
-            int nj=j+d[1];
-            if(ni<0 || ni>=n || nj<0 || nj>=n || arr[ni][nj]!=1) continue;
-            size += dfs(arr,ni,nj,n,uniqueId);
+    int parent[];
+    int size[];
+    int find(int a){
+        if(parent[a]==a) return a;
+        return parent[a]=find(parent[a]);
+    }
+    void union(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a!=b){
+            if(size[a]>size[b]){
+                parent[b]=a;
+                size[a]+=size[b];
+            }
+            else{
+                parent[a]=b;
+                size[b]+=size[a];
+            }
         }
-        return size;
     }
     public int largestIsland(int[][] arr) {
         int n=arr.length;
-        // boolean visited[][] = new boolean[n][n];
-        Map<Integer,Integer> map = new HashMap<>();
-        int uniqueId=2;
+        parent = new int[n*n];
+        size = new int[n*n];
+        for(int i=0;i<n*n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(arr[i][j]==1){
-                    int size=dfs(arr,i,j,n,uniqueId);
-                    map.put(uniqueId,size);
-                    uniqueId++;
+                    for(int d[]:dir){
+                        int ni=i+d[0];
+                        int nj=j+d[1];
+                        if(ni<0 || ni>=n || nj<0 || nj>=n || arr[ni][nj]!=1) continue;
+                        int nodeNo = i*n + j;
+                        int adjnodeNo = ni*n + nj;
+                        union(nodeNo,adjnodeNo);
+                    }
                 }
             }
         }
@@ -29,22 +46,22 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(arr[i][j]==0){
-                    arr[i][j]=1;
-                    int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
                     Set<Integer> set = new HashSet<>();
-                    int size=1;
                     for(int d[]:dir){
                         int ni=i+d[0];
                         int nj=j+d[1];
-                        if(ni<0 || ni>=n || nj<0 || nj>=n || arr[ni][nj]<=1 || set.contains(arr[ni][nj])) continue;
-                        size+=map.get(arr[ni][nj]);
-                        set.add(arr[ni][nj]);
+                        if(ni<0 || ni>=n || nj<0 || nj>=n || arr[ni][nj]!=1) continue;
+                        int adjnodeNo = ni*n + nj;
+                        set.add(find(adjnodeNo));
                     }
-                    arr[i][j]=0;
-                    max=Math.max(max,size);
+                    int total=1;
+                    for(int ele : set){
+                        total+=size[ele];
+                    }
+                    max=Math.max(max,total);
                 }
             }
         }
-        return max==0?n*n:max;
+        return max==0 ? n*n : max;
     }
 }
