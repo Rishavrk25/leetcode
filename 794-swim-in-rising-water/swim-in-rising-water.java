8cdd1b07b9b1e450318d636e07cc3e9042fe6502
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-12 18:22:35 +0530

    Time: 12 ms (55.02%) | Memory: 46.5 MB (61.51%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -2,25 +2,24 @@ class Solution {
     public int swimInWater(int[][] arr) {
         int n=arr.length;
         PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->Integer.compare(a[2],b[2]));
-        q.add(new int[]{0,0,arr[0][0]}); // i,j,time
-        int directions[][]={{0,1},{0,-1},{1,0},{-1,0}};
+        q.add(new int[]{0,0,arr[0][0]}); // i,j,wt
         int dist[][] = new int[n][n];
-        for(int a[]:dist) Arrays.fill(a,Integer.MAX_VALUE);
+        for(int a[] : dist) Arrays.fill(a,Integer.MAX_VALUE);
         dist[0][0]=arr[0][0];
+        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
         while(!q.isEmpty()){
-            int t[] = q.remove();
+            int t[]=q.remove();
             int i=t[0];
             int j=t[1];
-            int time=t[2];
-            if(time>dist[i][j]) continue;
-            for(int dir[]:directions){
-                int i_ = i+dir[0];
-                int j_ = j+dir[1];
-                if(i_<0 || i_>=n || j_<0 || j_>=n) continue;
-                int total = Math.max(time,arr[i_][j_]);
-                if(total < dist[i_][j_]){
-                    dist[i_][j_]=total;
-                    q.add(new int[]{i_,j_,total});
+            int cost=t[2];
+            for(int d[]:dir){
+                int ni=i+d[0];
+                int nj=j+d[1];
+                if(ni<0 || ni>=n || nj<0 || nj>=n) continue;
+                int total = Math.max(cost,arr[ni][nj]);
+                if(total < dist[ni][nj]){
+                    dist[ni][nj]=total;
+                    q.add(new int[]{ni,nj,total});
                 }
             }
         }
