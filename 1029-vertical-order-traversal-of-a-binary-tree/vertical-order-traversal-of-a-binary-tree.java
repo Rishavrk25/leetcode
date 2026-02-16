061dde61a7f65b987d91298b1c0213b595bf17dc
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-16 21:46:29 +0530

    Time: 4 ms (63.71%) | Memory: 44 MB (88.08%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -13,27 +13,51 @@
  *     }
  * }
  */
-class Solution {
-    void helper(TreeNode root,int idx,int level,PriorityQueue<int[]> q){
-        if(root==null) return;
-        q.add(new int[]{root.val,idx,level});
-        helper(root.left,idx-1,level+1,q);
-        helper(root.right,idx+1,level+1,q);
+class Triplet{
+    TreeNode node;
+    int idx;
+    int level;
+    Triplet(TreeNode node,int idx,int level){
+        this.node=node;
+        this.idx=idx;
+        this.level=level;
     }
+}
+class Solution {
+    // void helper(TreeNode root,int idx,int level,PriorityQueue<int[]> q){
+    //     if(root==null) return;
+    //     q.add(new int[]{root.val,idx,level});
+    //     helper(root.left,idx-1,level+1,q);
+    //     helper(root.right,idx+1,level+1,q);
+    // }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         // min heap --> (node,idx,level)
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->{
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
             if(a[1]==b[1] && a[2]==b[2]) return Integer.compare(a[0],b[0]); // idx,level
             if(a[1]==b[1]) return Integer.compare(a[2],b[2]); // idx
             else return Integer.compare(a[1],b[1]);
         }); 
-        helper(root,0,0,q);
+        // helper(root,0,0,q);
+        Queue<Triplet> q = new LinkedList<>();
+        q.add(new Triplet(root,0,0));
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Triplet t = q.remove();
+                TreeNode node = t.node;
+                int idx = t.idx;
+                int level = t.level;
+                pq.add(new int[]{node.val,idx,level});
+                if(node.left!=null) q.add(new Triplet(node.left,idx-1,level+1));
+                if(node.right!=null) q.add(new Triplet(node.right,idx+1,level+1));
+            }
+        }
         List<List<Integer>> ans = new ArrayList<>();
-        int top[] = q.peek();
+        int top[] = pq.peek();
         int minIdx = top[1];
         List<Integer> list = new ArrayList<>();
-        while(!q.isEmpty()){
-            int t[] = q.remove();
+        while(!pq.isEmpty()){
+            int t[] = pq.remove();
             int idx=t[1];
             if(idx==minIdx) list.add(t[0]);
             else{
@@ -45,6 +69,5 @@ class Solution {
         }
         ans.add(list);
         return ans;
-
     }
 }
