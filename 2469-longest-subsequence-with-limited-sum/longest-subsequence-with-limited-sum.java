7ac395505cf57a58188063774b5880907c10a808
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-19 21:00:01 +0530

    Time: 8 ms (92.47%) | Memory: 47.2 MB (28.34%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -1,15 +1,24 @@
 class Solution {
+    int bs(int pre[],int t){
+        int l=0;
+        int h=pre.length;
+        while(l<h){
+            int m = l + (h-l)/2;
+            if(pre[m]<=t) l=m+1;
+            else h=m;
+        }
+        return h;
+    }
     public int[] answerQueries(int[] arr, int[] queries) {
         Arrays.sort(arr);
         int n=arr.length;
         int m=queries.length;
+        // convert arr to pre
         for(int i=1;i<n;i++){
             arr[i]+=arr[i-1];
         }
         for(int i=0;i<m;i++){
-            int j=0;
-            while(j<n && queries[i]>=arr[j]) j++;
-            queries[i]=j;
+            queries[i]=bs(arr,queries[i]);
         }
         return queries;
     }
