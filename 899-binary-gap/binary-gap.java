b0c8d059d969943d035e9b4b9321e2e6649ed99b
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-22 22:17:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (67.94%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -1,13 +1,14 @@
 class Solution {
     public int binaryGap(int n) {
         int max=0;
-        int c=0;
-        while((n&1)==0) n=n>>1;
-        n=n>>1;
+        int idx=0;
+        int last =-1;
         while(n>0){
-            if((n&1)==1) c=0;
-            else c++;
-            max=Math.max(max,c+1);
+            if((n&1)==1){
+                if(last!=-1) max=Math.max(max,idx-last);
+                last=idx;
+            }
+            idx++;
             n=n>>1;
         }
         return max;
