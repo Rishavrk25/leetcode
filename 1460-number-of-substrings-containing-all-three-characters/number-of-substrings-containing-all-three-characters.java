40d457af10dea9d46524b62293a3ea51d0099a41
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-02-25 00:51:42 +0530

    Time: 13 ms (71.84%) | Memory: 46.2 MB (79.49%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -9,11 +9,10 @@ class Solution {
             char ch = s.charAt(j);
             freq[ch-'a']++;
             while(freq[0]>0 && freq[1]>0 && freq[2]>0){
-                c+=n-j;
                 freq[s.charAt(i)-'a']--;
                 i++;
             }
-            // c+=i;
+            c+=i;
             j++;
         }
         return c;
