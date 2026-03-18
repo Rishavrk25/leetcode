9156c3a46f7dc48d2801c30bcf17209518e9f0f5
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-18 11:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (70.06%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,6 +1,7 @@
 class Solution {
     int helper(int arr[],int n,int dp[]){
         if(n<0) return 0;
+        if(n==0) return arr[0];
         if(dp[n]!=-1) return dp[n];
         int skip = helper(arr,n-1,dp);
         int take = arr[n]+helper(arr,n-2,dp);
@@ -11,5 +12,6 @@ class Solution {
         int dp[] = new int[n+1];
         Arrays.fill(dp,-1);
         return helper(nums,n-1,dp);
+        
     }
 }
