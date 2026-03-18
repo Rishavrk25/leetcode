d23d253b404be6d7ccff82f71d32678371ec5c5c
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-18 16:31:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (56.00%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,29 +1,19 @@
 class Solution {
-    // int helper(int arr[],int s,int n,int dp[]){
-    //     if(n==s) return arr[s];
-    //     if(n==s+1) return Math.max(arr[s],arr[s+1]);
-    //     if(dp[n]!=-1) return dp[n];
-    //     int skip = helper(arr,s,n-1,dp);
-    //     int take = arr[n]+helper(arr,s,n-2,dp);
-    //     return dp[n]=Math.max(skip,take);
-    // }
     int helper(int arr[],int s,int n){
-        int prev2 = arr[s];
+        int dp[] = new int[arr.length];
+        dp[s] = arr[s];
         if(s==n) return arr[s];
-        int prev = Math.max(arr[s],arr[s+1]);
+        dp[s+1] = Math.max(arr[s],arr[s+1]);
         for(int i=s+2;i<=n;i++){
-            int skip = prev;
-            int take = arr[i] + prev2;
-            int curr = Math.max(skip,take);
-            prev2=prev;
-            prev=curr;
+            int skip = dp[i-1];
+            int take = arr[i] + dp[i-2];
+            dp[i] = Math.max(skip,take);
         }
-        return prev;
+        return dp[n];
     }
     public int rob(int[] arr) {
         int n=arr.length;
         if(n==1) return arr[0];
-        // int dp[] = new int[n];
         int a=helper(arr,0,n-2);
         int b=helper(arr,1,n-1);
         return Math.max(a,b);
