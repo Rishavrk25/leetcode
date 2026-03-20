ea5a0b40c0ab822960ce745475b8d0d08f562061
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-20 18:12:31 +0530

    Time: 36 ms (29.79%) | Memory: 47.3 MB (95.74%) - LeetSync

---
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] minAbsDiff(int[][] arr, int k) {
+        int n=arr.length;
+        int m=arr[0].length;
+        int res[][] = new int[n-k+1][m-k+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int i_ = i+k-1;
+                int j_ = j+k-1;
+                if(i_<n && j_<m){
+                    Set<Integer> set = new HashSet<>();
+                    for(int s=i;s<=i_;s++){
+                        for(int t=j;t<=j_;t++){
+                            set.add(arr[s][t]);
+                        }
+                    }
+                    List<Integer> list = new ArrayList<>(set);
+                    Collections.sort(list);
+                    int min = Integer.MAX_VALUE;
+                    for(int s=1;s<list.size();s++){
+                        int diff = Math.abs(list.get(s) - list.get(s-1));
+                        min = Math.min(min,diff);
+                    }
+                    if(min==Integer.MAX_VALUE) min=0;
+                    res[i][j]=min;
+                }
+            }
+        }
+        
+        if(k==1) return res;
+        return res;
+    }
+}
