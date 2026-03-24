f769ab4c904c9392c9897e4013e1694b6b472f21
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-24 23:17:28 +0530

    Time: 14 ms (14.71%) | Memory: 87.6 MB (97.55%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.java
+++ 3031-construct-product-matrix/construct-product-matrix.java
@@ -2,35 +2,25 @@ class Solution {
     public int[][] constructProductMatrix(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        long arr[] = new long[m*n];
-        long pre[] = new long[m*n];
-        long suf[] = new long[m*n];
-        int k=0;
+        long pre[][] = new long[m][n];
+        long suf[][] = new long[m][n];
+        long p = 1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                arr[k]=grid[i][j];
-                pre[k]=grid[i][j];
-                suf[k]=grid[i][j];
-                k++;
+                pre[i][j] = p;
+                p =(p * grid[i][j]) % 12345;
             }
         }
-        for(int i=1;i<m*n;i++){
-            pre[i]= (pre[i]*pre[i-1]) % 12345;
+        long s = 1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                suf[i][j] = s;
+                s = (s * grid[i][j]) % 12345;
+            }
         }
-        for(int i=m*n-2;i>=0;i--){
-            suf[i]= (suf[i]*suf[i+1]) % 12345;
-        }
-        for(int i=0;i<m*n;i++){
-            long a=1;
-            long b=1;
-            if(i-1>=0) a=pre[i-1];
-            if(i+1<m*n) b=suf[i+1];
-            arr[i] =(a * b) % 12345;
-        }
-        k=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                grid[i][j] = (int)arr[k++];
+                grid[i][j] = (int)(pre[i][j]*suf[i][j]) % 12345;
             }
         }
         return grid;
