54fc4a9034a39ffde6bc8b2c84291e7140f618dd
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-25 00:01:13 +0530

    Time: 3 ms (4.81%) | Memory: 44.1 MB (7.61%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,24 +1,41 @@
 class Solution {
-    int helper(int arr[][],int i,int j,int dp[][]){
-        if(i==0 && j==0) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int dir[][] = {{0,-1},{-1,0}};
-        int ans = 0;
-        for(int d[]: dir){
-            int i_ = i + d[0];
-            int j_ = j + d[1];
-            if(i_>=0 && j_>=0 && arr[i_][j_]!=1){
-                ans += helper(arr,i_,j_,dp);
-            }
-        }
-        return dp[i][j]=ans;
-    }
+    // int helper(int arr[][],int i,int j,int dp[][]){
+    //     if(i==0 && j==0) return 1;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int dir[][] = {{0,-1},{-1,0}};
+    //     int ans = 0;
+    //     for(int d[]: dir){
+    //         int i_ = i + d[0];
+    //         int j_ = j + d[1];
+    //         if(i_>=0 && j_>=0 && arr[i_][j_]!=1){
+    //             ans += helper(arr,i_,j_,dp);
+    //         }
+    //     }
+    //     return dp[i][j]=ans;
+    // }
     public int uniquePathsWithObstacles(int[][] arr) {
         int m=arr.length;
         int n=arr[0].length;
         if(arr[m-1][n-1]==1) return 0;
         int dp[][] = new int[m][n];
-        for(int row[] : dp) Arrays.fill(row,-1);
-        return helper(arr,m-1,n-1,dp);
+        // for(int row[] : dp) Arrays.fill(row,-1);
+        // return helper(arr,m-1,n-1,dp);
+        dp[0][0] = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                int dir[][] = {{0,-1},{-1,0}};
+                int ans = 0;
+                for(int d[] : dir){
+                    int i_ = i+d[0];
+                    int j_ = j+d[1];
+                    if(i_>=0 && j_>=0 && arr[i_][j_]!=1){
+                        ans += dp[i_][j_];
+                    }
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
