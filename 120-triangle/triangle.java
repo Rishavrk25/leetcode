db2564abd573f16f0f28ea3b77fc14aef85468de
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-26 16:28:10 +0530

    Time: 2 ms (87.83%) | Memory: 45.5 MB (58.17%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,18 +1,22 @@
 class Solution {
-    int mod = -((int)10e4-1);
+    int mod = -((int)1e4-1);
     int helper(List<List<Integer>> arr,int i,int j,int dp[][]){
-        int n = arr.size();
+        if(i<0 || j<0 || j>i) return -mod;
+        if(i==0 && j==0) return arr.get(0).get(0);
         if(dp[i][j]!=mod) return dp[i][j];
-        if(i==n-1) return arr.get(i).get(j);
-        int a = arr.get(i).get(j) + helper(arr,i+1,j,dp);
-        int b = arr.get(i).get(j) + helper(arr,i+1,j+1,dp);
+        int a = arr.get(i).get(j) + helper(arr,i-1,j,dp);
+        int b = arr.get(i).get(j) + helper(arr,i-1,j-1,dp);
         return dp[i][j]=Math.min(a,b);
-        
     }
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
+    public int minimumTotal(List<List<Integer>> arr) {
+        int ans = Integer.MAX_VALUE;
+        int n = arr.size();
         int dp[][] = new int[n][n];
         for(int row[]:dp) Arrays.fill(row,mod);
-        return helper(triangle,0,0,dp);
+        List<Integer> list = arr.get(n-1);
+        for(int i=0;i<list.size();i++){
+            ans = Math.min(ans,helper(arr,n-1,i,dp));
+        }
+        return ans;
     }
 }
