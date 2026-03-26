41e72134bc996f64294dfd3368a70907d422c27f
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-26 14:32:59 +0530

    Time: 4 ms (97.20%) | Memory: 160.6 MB (75.52%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -3,26 +3,14 @@ class Solution {
         int m = arr.length;
         int n = arr[0].length;
         long total = 0;
+        long rowsum[] = new long[m];
+        long colsum[] = new long[n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 total+=arr[i][j];
+                rowsum[i] +=arr[i][j];
+                colsum[j] +=arr[i][j];
             }
-        }
-        long rowsum[] = new long[m];
-        for(int i=0;i<m;i++){
-            int s=0;
-            for(int j=0;j<n;j++){
-                s+=arr[i][j];
-            }
-            rowsum[i] = s;
-        }
-        long colsum[] = new long[n];
-        for(int j=0;j<n;j++){
-            int s=0;
-            for(int i=0;i<m;i++){
-                s+=arr[i][j];
-            }
-            colsum[j] = s;
         }
         // row partition
         long sum=0;
