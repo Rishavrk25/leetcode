e3a24b147d22f26dcf4f348122a92b5c9b1570ff
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-28 15:49:34 +0530

    Time: 125 ms (6.83%) | Memory: 123.2 MB (5.72%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,12 +1,12 @@
 class Solution {
     boolean helper(int arr[],int i,int sum,int total,int dp[][]){
-        if(i==arr.length){
+        if(i<0){
             if(sum==total-sum) return true;
             else return false;
         }
         if(dp[i][sum]!=-1) return dp[i][sum]==1;
-        boolean take = helper(arr,i+1,sum+arr[i],total,dp);
-        boolean skip = helper(arr,i+1,sum,total,dp);
+        boolean take = helper(arr,i-1,sum+arr[i],total,dp);
+        boolean skip = helper(arr,i-1,sum,total,dp);
         boolean ans = take || skip;
         dp[i][sum]=ans?1:0;
         return ans;
@@ -18,6 +18,6 @@ class Solution {
         int n=nums.length;
         int dp[][] = new int[n][total];
         for(int row[]:dp) Arrays.fill(row,-1);
-        return helper(nums,0,0,total,dp);
+        return helper(nums,n-1,0,total,dp);
     }
 }
