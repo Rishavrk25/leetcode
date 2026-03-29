f36ae0bcb4e97d3d3e1f56ee8e2f28d445ff07b3
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-29 10:45:01 +0530

    Time: 85 ms (27.83%) | Memory: 123.5 MB (5.28%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,29 +1,30 @@
 class Solution {
-    boolean helper(int arr[],int i,int sum,int total,int dp[][]){
-        if(i==0) return sum==total-sum;
-        if(dp[i][sum]!=-1) return dp[i][sum]==1;
+    boolean helper(int arr[],int i,int target,int dp[][]){
+        if(i==0) return target==0;
+        if(dp[i][target]!=-1) return dp[i][target]==1;
         boolean take = false;
-        if(sum+arr[i]<=total) take=helper(arr,i-1,sum+arr[i],total,dp);
-        boolean skip = helper(arr,i-1,sum,total,dp);
+        if(target-arr[i]>=0) take = helper(arr,i-1,target-arr[i],dp);
+        boolean skip = helper(arr,i-1,target,dp);
         boolean ans = take || skip;
-        dp[i][sum]=ans?1:0;
+        dp[i][target]=ans?1:0;
         return ans;
     }
     public boolean canPartition(int[] nums) {
         int total=0;
         for(int ele:nums) total+=ele;
         if(total%2!=0) return false;
+        int target = total/2;
         int n=nums.length;
         int dp[][] = new int[n][total];
         for(int row[]:dp) Arrays.fill(row,-1);
-        return helper(nums,n-1,0,total,dp);
+        return helper(nums,n-1,target,dp);
         // for(int j=0;j<total;j++){
         //     if(j == total-j) dp[0][j]=1;
         // }
         // for(int i=1;i<n;i++){
         //     for(int j=0;j<total;j++){
         //         int take = 0;
-        //         if(j+nums[i]<total) take = dp[i-1][j+nums[i]];
+        //         if(j+nums[i]>0) take = dp[i-1][j+nums[i]];
         //         int skip = dp[i-1][j];
         //         boolean ans = (take==1) || (skip==1);
         //         dp[i][j] = ans?1:0;
