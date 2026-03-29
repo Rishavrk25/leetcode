dabf5f85882d48fe16735eef95dd27d7f7fb2f8d
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-29 10:53:23 +0530

    Time: 78 ms (31.43%) | Memory: 76.3 MB (10.92%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     boolean helper(int arr[],int i,int target,int dp[][]){
-        if(i==0) return target==0;
+        if(target==0) return true;
+        if(i==0) return arr[i]==target;
         if(dp[i][target]!=-1) return dp[i][target]==1;
         boolean take = false;
         if(target-arr[i]>=0) take = helper(arr,i-1,target-arr[i],dp);
@@ -19,16 +20,17 @@ class Solution {
         for(int row[]:dp) Arrays.fill(row,-1);
         return helper(nums,n-1,target,dp);
         // dp[0][0]=1;
-        // for(int i=1;i<n;i++){
-        //     for(int j=0;j<total;j++){
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<target;j++){
+        //         if(i==0 && j==0) continue;
         //         int take = 0;
-        //         if(j+nums[i]>0) take = dp[i-1][j+nums[i]];
+        //         if(j-nums[i]>=0) take = dp[i-1][j-nums[i]];
         //         int skip = dp[i-1][j];
         //         boolean ans = (take==1) || (skip==1);
         //         dp[i][j] = ans?1:0;
         //     }
         // }
-        // return dp[n-1][total-1]==1;
+        // return dp[n-1][target]==1;
 
     }
 }
