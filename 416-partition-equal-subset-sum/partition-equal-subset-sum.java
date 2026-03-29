2b96bcbe231dd1c9773e69920a68bd887e55ff0c
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-03-29 10:55:27 +0530

    Time: 46 ms (58.90%) | Memory: 75.7 MB (18.05%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     boolean helper(int arr[],int i,int target,int dp[][]){
         if(target==0) return true;
-        if(i==0) return arr[i]==target;
+        if(i==0) return arr[0]==target;
         if(dp[i][target]!=-1) return dp[i][target]==1;
         boolean take = false;
         if(target-arr[i]>=0) take = helper(arr,i-1,target-arr[i],dp);
@@ -17,20 +17,22 @@ class Solution {
         int target = total/2;
         int n=nums.length;
         int dp[][] = new int[n][target+1];
-        for(int row[]:dp) Arrays.fill(row,-1);
-        return helper(nums,n-1,target,dp);
-        // dp[0][0]=1;
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<target;j++){
-        //         if(i==0 && j==0) continue;
-        //         int take = 0;
-        //         if(j-nums[i]>=0) take = dp[i-1][j-nums[i]];
-        //         int skip = dp[i-1][j];
-        //         boolean ans = (take==1) || (skip==1);
-        //         dp[i][j] = ans?1:0;
-        //     }
-        // }
-        // return dp[n-1][target]==1;
+        // for(int row[]:dp) Arrays.fill(row,-1);
+        // return helper(nums,n-1,target,dp);
+        for(int i=0;i<n;i++) dp[i][0]=1;
+        for(int j=0;j<target+1;j++){
+            if(nums[0]==j) dp[0][j]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<target+1;j++){
+                int take = 0;
+                if(j-nums[i]>=0) take = dp[i-1][j-nums[i]];
+                int skip = dp[i-1][j];
+                boolean ans = (take==1) || (skip==1);
+                dp[i][j] = ans?1:0;
+            }
+        }
+        return dp[n-1][target]==1;
 
     }
 }
