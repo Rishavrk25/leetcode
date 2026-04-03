4f0bb8a35768149d08420755c061ae5d26497a87
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-04-03 14:41:12 +0530

    Time: 26 ms (28.89%) | Memory: 46.8 MB (38.29%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,23 +1,21 @@
 class Solution {
-    int helper(int arr[],int i,int t,int dp[][]){
-        // if(i==arr.length){
-        //     if(t==0) return 0;
-        //     else return (int)1e5;
-        // }
-        if(t==0) return 0;
-        if(i==arr.length) return (int)1e5;
-        if(dp[i][t]!=-1) return dp[i][t];
-        int take = (int)1e5;
-        if(t-arr[i]>=0) take = 1 + helper(arr,i,t-arr[i],dp);
-        int skip = helper(arr,i+1,t,dp);
-        return dp[i][t]=Math.min(take,skip);
+    int helper(int arr[],int amount,int i,int dp[][]){
+        if(i==0){
+            if(amount%arr[0]==0) return amount/arr[0];
+            else return (int)1e9;
+        }
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int take = (int)1e9;
+        if(amount-arr[i]>=0) take = 1+helper(arr,amount-arr[i],i,dp);
+        int skip = helper(arr,amount,i-1,dp);
+        return dp[i][amount] = Math.min(take,skip);
     }
-    public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
+    public int coinChange(int[] arr, int amount) {
+        int n=arr.length;
         int dp[][] = new int[n][amount+1];
         for(int row[]:dp) Arrays.fill(row,-1);
-        int ans = helper(coins,0,amount,dp);
-        if(ans==(int)1e5) return -1;
-        else return ans;
+        int ans=helper(arr,amount,arr.length-1,dp);
+        if(ans==(int)1e9) return -1;
+        return ans;
     }
 }
