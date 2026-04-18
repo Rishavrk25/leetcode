eccb59224aa5080750c2dd56a4bc79217b51f9da
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-04-18 11:53:17 +0530

    Time: 100 ms (16.90%) | Memory: 46.7 MB (21.90%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -4,13 +4,13 @@ class Solution {
         int dp[] = new int[n];
         int ans = 0;
         for(int i=0;i<n;i++){
-            int max = 1;
+            int max = 0;
             for(int j=0;j<i;j++){
                 if(arr[j]<arr[i]){
-                    max = Math.max(max,dp[j]+1);
+                    max = Math.max(max,dp[j]);
                 }
             }
-            dp[i]=max;
+            dp[i]=max+1;
             ans = Math.max(ans,dp[i]);
         }
         for(int ele:dp) System.out.print(ele+" ");
