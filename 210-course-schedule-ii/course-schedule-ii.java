ddfd406b73f4f706a87d9f90916bd8e34ff59f50
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-04-19 11:07:14 +0530

    Time: 6 ms (49.00%) | Memory: 47 MB (38.33%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,39 +1,31 @@
 class Solution {
-    boolean isCycle(List<List<Integer>> adj,int i,List<Integer> list,boolean visited[],boolean inRecursion[]){
-        visited[i]=true;
-        inRecursion[i]=true;
-        for(int nei:adj.get(i)){
-            if(!visited[nei]){
-                if(isCycle(adj,nei,list,visited,inRecursion)) return true;
-            }
-            else if(inRecursion[nei]) return true; 
-        }
-        list.add(0,i);
-        inRecursion[i]=false;
-        return false;
-    }
     public int[] findOrder(int n, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-        for(int arr[]:prerequisites){
-            int u=arr[0];
-            int v=arr[1];
-            adj.get(v).add(u);
+        int indegree[] = new int[n];
+        for(int p[]:prerequisites){
+            int u=p[1];
+            int v=p[0];
+            adj.get(u).add(v);
+            indegree[v]++;
         }
-        boolean visited[] = new boolean[n];
-        boolean inRecursion[] = new boolean[n];
-        List<Integer> list = new ArrayList<>();
-        boolean flag = false; // no cycle
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(!visited[i] && isCycle(adj,i,list,visited,inRecursion)){
-                flag = true; // cycle
-                break;
+            if(indegree[i]==0) q.add(i);
+        }
+        int ans[] = new int[n];
+        int k=0;
+        int c=0;
+        while(!q.isEmpty()){
+            int u = q.remove();
+            c++;
+            ans[k++]=u;
+            for(int nei:adj.get(u)){
+                indegree[nei]--;
+                if(indegree[nei]==0) q.add(nei);
             }
         }
-        if(flag) return new int[]{};
-        int ans[] = new int[n];
-        for(int i=0;i<n;i++) ans[i]=list.get(i);
+        if(c!=n) return new int[]{};
         return ans;
-
     }
 }
