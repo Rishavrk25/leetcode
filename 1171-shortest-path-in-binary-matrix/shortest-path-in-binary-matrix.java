134f548a7162608a6093063e213c288dc2359160
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-04-19 11:23:53 +0530

    Time: 18 ms (32.69%) | Memory: 47.7 MB (48.13%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,29 +1,32 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] arr) {
-        int n=arr.length;
-        int m=arr[0].length;
-        if(arr[0][0]!=0) return -1;
+        int m=arr.length;
+        int n=arr[0].length;
+        if(arr[0][0]==1 || arr[m-1][n-1]==1) return -1;
+        int dir[][] = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{-1,1},{1,-1}};
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0,0});
-        int direction[][] = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{-1,1},{1,-1}};
-        int dist[][] = new int[n][m];
-        for(int a[]:dist) Arrays.fill(a,Integer.MAX_VALUE);
-        dist[0][0]=1;
+        boolean visited[][] = new boolean[m][n];
+        visited[0][0]=true;
+        int c=1;
         while(!q.isEmpty()){
-            int p[] = q.remove();
-            int i=p[0];
-            int j=p[1];
-            for(int dir[]:direction){
-                int ni = i+dir[0];
-                int nj = j+dir[1];
-                if(ni<0 || nj<0 || ni>=n || nj>=m || arr[ni][nj]!=0) continue;
-                if(dist[i][j] + 1 < dist[ni][nj]){
-                    dist[ni][nj] = dist[i][j] + 1;
-                    q.add(new int[]{ni,nj});
+            int size = q.size();
+            while(size-->0){
+                int p[]=q.remove();
+                int i=p[0];
+                int j=p[1];
+                if(i==m-1 && j==n-1) return c;
+                for(int d[]:dir){
+                    int ni=i+d[0];
+                    int nj=j+d[1];
+                    if(ni>=0 && nj>=0 && ni<m && nj<n && !visited[ni][nj] && arr[ni][nj]==0){
+                        q.add(new int[]{ni,nj});
+                        visited[ni][nj]=true;
+                    }
                 }
             }
+            c++;
         }
-        if(dist[n-1][m-1]==Integer.MAX_VALUE) return -1;
-        else return dist[n-1][m-1];
+        return -1;
     }
 }
