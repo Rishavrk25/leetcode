3280361b574062eb88680144e028f120c96703ce
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-04-20 16:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (48.63%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,17 +1,5 @@
 class Solution {
-    double helper(double x,int n){
-        if(n==0) return 1;
-        double ans = helper(x,n/2);
-        if(n%2==0) return ans*ans;
-        else return ans*ans*x;
-    }
-    
     public double myPow(double x, int n) {
-        // double N = n;
-        if(n<0){
-            x=1.0/x;
-            n=-n;
-        }
-        return helper(x,n);
+        return Math.pow(x,n);
     }
 }
