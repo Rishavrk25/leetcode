a9166a55facea26500d8b5e314063feca016982a
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-04-23 22:23:41 +0530

    Time: 60 ms (13.90%) | Memory: 129.4 MB (11.66%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.java
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -6,31 +6,35 @@ class Solution {
         long ans[] = new long[n];
         for(int i=0;i<n;i++){
             if(!mapfreq.containsKey(arr[i])){
-                mapfreq.put(arr[i],0);
-                mapsum.put(arr[i],0L);
+                mapfreq.put(arr[i],1);
+                mapsum.put(arr[i],(long)i);
             }
+            else{
+                long a = mapfreq.get(arr[i]);
+                long b = mapsum.get(arr[i]);
+                ans[i] += (long)a*i - b;
 
-            long a = mapfreq.get(arr[i]);
-            long b = mapsum.get(arr[i]);
-            ans[i] += (long)a*i - b;
+                mapfreq.put(arr[i],mapfreq.get(arr[i])+1);
+                mapsum.put(arr[i],mapsum.get(arr[i]) + i);
 
-            mapfreq.put(arr[i],mapfreq.get(arr[i])+1);
-            mapsum.put(arr[i],mapsum.get(arr[i]) + i);
+            }
             
         }
         mapfreq.clear();
         mapsum.clear();
         for(int i=n-1;i>=0;i--){
             if(!mapfreq.containsKey(arr[i])){
-                mapfreq.put(arr[i],0);
-                mapsum.put(arr[i],0L);
+                mapfreq.put(arr[i],1);
+                mapsum.put(arr[i],(long)i);
             }
-            long a = mapfreq.get(arr[i]);
-            long b = mapsum.get(arr[i]);
-            ans[i] += b - (long)a*i;
-            
-            mapfreq.put(arr[i],mapfreq.get(arr[i])+1);
-            mapsum.put(arr[i],mapsum.get(arr[i]) + i);
+            else{
+                long a = mapfreq.get(arr[i]);
+                long b = mapsum.get(arr[i]);
+                ans[i] += b - (long)a*i;
+                
+                mapfreq.put(arr[i],mapfreq.get(arr[i])+1);
+                mapsum.put(arr[i],mapsum.get(arr[i]) + i);
+            }
             
         }
         return ans;
