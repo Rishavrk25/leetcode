7f196ba5af3042e6b0511c98d02bb0af8c9f8d2a
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-05-05 17:31:16 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (44.03%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -9,29 +9,40 @@
  * }
  */
 class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
-        if(head==null) return head;
-
-        int size=0;
-        ListNode temp=head;
-        while(temp!=null){
-            temp=temp.next;
-            size++;
+    ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode Next=head;
+        while(curr!=null){
+            Next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=Next;
         }
-
+        return prev;
+    }
+    public ListNode rotateRight(ListNode head, int k) {
+        int size=0;
+        ListNode t=head;
+        while(t!=null){
+            size++;
+            t=t.next;
+        }
+        if(size==0) return head;
         k=k%size;
         if(k==0) return head;
-
-        ListNode slow=head;
-        ListNode fast=head;
-        for(int i=0;i<k;i++) fast=fast.next;
-        while(fast.next!=null){
-            slow=slow.next;
-            fast=fast.next;
-        }
-        ListNode newHead = slow.next;
-        slow.next=null;
-        fast.next=head;
-        return newHead;
+        head = reverse(head);
+        ListNode l1 = head;
+        ListNode temp = head;
+        for(int i=0;i<k-1;i++) temp=temp.next;
+        ListNode l2=null;
+        if(temp!=null) l2=temp.next;
+        if(temp!=null) temp.next=null;
+        l1=reverse(l1);
+        l2=reverse(l2);
+        temp=l1;
+        while(temp.next!=null) temp=temp.next;
+        temp.next=l2;
+        return l1;
     }
 }
