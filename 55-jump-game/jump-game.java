311dc5ee30474f1fd043443f847aa30e97503953
Rishavrk25 <139035930+Rishavrk25@users.noreply.github.com>
2026-05-07 22:36:59 +0530

    Time: 382 ms (5.05%) | Memory: 48.1 MB (22.81%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,47 +1,23 @@
 class Solution {
-    boolean helper(int arr[],int i,int n,int dp[]){
-        if(i==n-1) return true;
+    boolean helper(int arr[],int i,int dp[]){
+        if(i>=arr.length) return false;
+        if(i==arr.length-1) return true;
         if(dp[i]!=-1) return dp[i]==1;
-        boolean flag=false;
-        for(int j=1;j<=arr[i];j++){
-            flag=flag||helper(arr,i+j,n,dp);
+        int ele = arr[i];
+        boolean ans=false;
+        for(int j=1;j<=ele;j++){
+            if(helper(arr,i+j,dp)){
+                ans=true;
+                return true;
+            }
         }
-        dp[i]=flag?1:0;
-        return flag;
+        dp[i] = ans ? 1 : 0;
+        return ans;
     }
     public boolean canJump(int[] arr) {
         int n=arr.length;
-        int dp[]=new int[n];
+        int dp[]= new int[n];
         Arrays.fill(dp,-1);
-        return helper(arr,0,n,dp);
-        // Brute Force
-        // int n=arr.length;
-        // if(n==1) return true;
-        // boolean iszero=false;
-        // for(int i=0;i<n-1;i++) if(arr[i]==0) iszero=true;
-        // if(!iszero) return true;
-        // for(int i=0;i<n-1;i++){
-        //     if(arr[i]==0){
-        //         boolean flag=false;
-        //         for(int j=i-1;j>=0;j--){
-        //             if(j+arr[j]>i){
-        //                 flag=true;
-        //                 break;
-        //             }
-        //         }
-        //         if(!flag) return false;
-        //     }
-        // }
-        // return true;
-
-        // Greedy
-        // int n=arr.length;
-        // int maxIdx  = 0;
-        // for(int i=0;i<n;i++){
-        //     if(maxIdx<i) return false;
-        //     maxIdx = Math.max(maxIdx,i+arr[i]);
-        // }
-        // return true;
-        
+        return helper(arr,0,dp);
     }
 }
